d67b114c0cdf4b3ec1eb609cfb3fd4e50dbcbf20
Edwin Sandoval <esandovalv@correo.udistrital.edu.co>
2023-03-12 13:12:19 -0500

    First Commit

---
+++ PruebaTravel/PruebaTravel/Models/LibrosAutores.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PruebaTravel.Models
+{
+    public class LibrosAutores
+    {
+        public Int16 IdLibro { get; set; }
+        public string Autor { get; set; }
+        public string Titulo { get; set; }
+        public string Npaginas { get; set; }
+
+    }
+}
